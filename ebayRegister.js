d7070e46691293781a80311c4c140e95e7c5c16d
adil89189-ship-it <adil89189@gmail.com>
2025-12-27 17:48:26 +0500

    add required Content-Language header and finalize ebay inventory integration

--- ebayRegister.js
+++ ebayRegister.js
@@ -49,7 +49,8 @@ export async function registerSku(sku) {
       method: "PUT",
       headers: {
         Authorization: `Bearer ${token.access_token}`,
-        "Content-Type": "application/json"
+        "Content-Type": "application/json",
+        "Content-Language": "en-GB"
       },
       body: JSON.stringify({
         availability: {
